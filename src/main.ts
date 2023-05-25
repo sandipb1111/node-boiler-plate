c72520ee8e6821fae69518e57ea2d02a426e38cc
Sandip <sandip.basnet1111@gmail.com>
2023-05-25 07:50:00 +0545

    Boiler plate

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+// const express = require("express")
+// const app = express()
+// const port = 3000
+
+// app.get("/", (req: any, res: any) => {
+//     res.send("Hey There its express js")
+// })
+
+// app.get("/greet", (req: any, res: any) => {
+//     res.send("Hey There")
+// })
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
+// console.log("Yo")
+console.log("yo")
